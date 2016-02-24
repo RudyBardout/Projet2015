88d612694aa9c9c3c7a869cba9a67374861aa469
FelixBOUCHER <felixboucher@laposte.net>
2016-02-24 10:35:05 +0100

    Writing stuff for stats...

---
+++ src/fr/univavignon/courbes/stats/StockageStats.java
@@ -0,0 +1,54 @@
+package fr.univavignon.courbes.stats;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StockageStats {
+	
+	public static void writeStats(String player, String stat, long howMuch)
+	{
+		int whichLine = -1;
+		if(stat == "ELO") whichLine = 0;
+		if(stat == "playedGames") whichLine = 2;
+		if(stat == "wonGames") whichLine = 3;
+		if(stat == "playedRounds") whichLine = 4;
+		if(stat == "wonRounds") whichLine = 5;
+		if(stat == "pointsTotal") whichLine = 6;
+		//if(whichLine == -1) exit;
+		
+		try {
+			FileWriter fw = new FileWriter (player);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter saveFile = new PrintWriter (bw);
+			
+			InputStream ips = new FileInputStream(player); 
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			
+			while(whichLine > 0)
+			{
+				br.readLine();
+				whichLine --;
+			}
+			
+			String readed = br.readLine();
+			br.close();
+			long swappedStat = Long.parseLong(readed);
+			swappedStat = swappedStat+howMuch;
+			
+			saveFile.println(swappedStat);
+			saveFile.close();
+			}
+		
+		catch (Exception e) {System.out.println(e.toString());}	
+	}
+
+}
